92730142677f004c822b87c9b68b64d1fa7c58d9
levgren <jekaobodianskyi@gmail.com>
2019-08-16 11:57:50 +0300

    + lombok @ToString

--- src/main/java/comparatorTaskBiloleg/Persons.java
+++ src/main/java/comparatorTaskBiloleg/Persons.java
@@ -1,20 +1,17 @@
 package comparatorTaskBiloleg;
 
 import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
+@ToString
 public class Persons {
 
-    @Getter private String name;
+    @Getter @Setter private String name;
     @Getter private int age;
 
     Persons(String name, int age) {
         this.name = name;
         this.age = age;
-    }
-
-    @Override
-    public String toString() {
-        return  "name='" + name + '\'' +
-                ", age=" + age;
     }
 }
