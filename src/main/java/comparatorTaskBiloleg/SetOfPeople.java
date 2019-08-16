4cde5a804a6f9eff4ec9bc17e034dbebfa6e2857
levgren <jekaobodianskyi@gmail.com>
2019-08-16 12:15:18 +0300

    + lombok @ToString @Getter @AllArgsConstructor + change order in TreeSet

--- src/main/java/comparatorTaskBiloleg/SetOfPeople.java
+++ src/main/java/comparatorTaskBiloleg/SetOfPeople.java
@@ -13,21 +13,21 @@ public class SetOfPeople {
         TreeSet<Persons> people = new TreeSet<>(compare);
         people.add(new Persons("Andrey", 36));
         people.add(new Persons("Andrey", 35));
+        people.add(new Persons("Artiom", 42));
+        people.add(new Persons("Dima", 18));
+        people.add(new Persons("Ievgen", 33));
+        people.add(new Persons("Yulia", 28));
+        people.add(new Persons("Kolia", 25));
         people.add(new Persons("Andrey", 39));
         people.add(new Persons("Andrey", 42));
         people.add(new Persons("Artiom", 36));
         people.add(new Persons("Artiom", 35));
         people.add(new Persons("Artiom", 39));
-        people.add(new Persons("Artiom", 42));
         people.add(new Persons("Ievgen", 33));
-        people.add(new Persons("Ievgen", 33));
-        people.add(new Persons("Dima", 18));
         people.add(new Persons("Ivan", 44));
         people.add(new Persons("Dima", 27));
-        people.add(new Persons("Kolia", 25));
         people.add(new Persons("Kolia", 45));
         people.add(new Persons("Yulia", 30));
-        people.add(new Persons("Yulia", 28));
         people.add(new Persons("Yulia", 18));
 
         Iterator<Persons> itr = people.iterator();
