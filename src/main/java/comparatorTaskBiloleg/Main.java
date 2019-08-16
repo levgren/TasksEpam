85255332862651d988201f44dce114dca46030bd
levgren <jekaobodianskyi@gmail.com>
2019-08-16 10:52:19 +0300

    +reversed & iterator

--- src/main/java/comparatorTaskBiloleg/Main.java
+++ src/main/java/comparatorTaskBiloleg/Main.java
@@ -1,25 +1,39 @@
 package comparatorTaskBiloleg;
 
+import com.sun.org.apache.xalan.internal.xslt.Process;
+
 import java.util.Comparator;
+import java.util.Iterator;
 import java.util.TreeSet;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        Comparator<Persons> compare = new CompareNames().thenComparing(new CompareAge());
+        Comparator<Persons> compare = new CompareNames().thenComparing(new CompareAge().reversed());
 
         TreeSet<Persons> people = new TreeSet<>(compare);
         people.add(new Persons("Andrey", 36));
+        people.add(new Persons("Andrey", 35));
         people.add(new Persons("Andrey", 39));
+        people.add(new Persons("Andrey", 42));
+        people.add(new Persons("Artiom", 36));
+        people.add(new Persons("Artiom", 35));
+        people.add(new Persons("Artiom", 39));
+        people.add(new Persons("Artiom", 42));
         people.add(new Persons("Ievgen", 33));
         people.add(new Persons("Ievgen", 33));
         people.add(new Persons("Dima", 18));
+        people.add(new Persons("Ivan", 44));
+        people.add(new Persons("Dima", 27));
         people.add(new Persons("Kolia", 25));
-        people.add(new Persons("Koli", 45));
+        people.add(new Persons("Kolia", 45));
         people.add(new Persons("Yulia", 30));
+        people.add(new Persons("Yulia", 28));
         people.add(new Persons("Yulia", 18));
 
-        System.out.println(people);
+        for (Persons person : people)
+            System.out.println(person);
+        }
     }
-}
+
